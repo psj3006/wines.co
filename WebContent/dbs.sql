57d278b03c27e33afb116185b956beee1f94c04b
abcdef544321 <psj3006@naver.com>
2019-07-25 20:29:01 +0900

    no message

--- WebContent/dbs.sql
+++ WebContent/dbs.sql
@@ -1,13 +1,13 @@
+drop table member;
+
 create table member (
 	id varchar2(20) primary key,
 	pw varchar2(20) not null,
 	name varchar2(10) not null,
 	addr varchar2(100) not null,
-	tel varchar2(11) not null
-	admin boolean 
+	tel varchar2(11) not null,
+	admin char(1) check( admin in('0', '1') )
 );
 
-insert into member values ()
-
-create table product
+insert into member values ('admin', '1111', 'admin', 'admin', 'admin', '1');
 
